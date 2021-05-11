a2f59defae9c8c72c3a671fab61d32a0ab8a61ec
outsidecoder <neill.hunter@gmail.com>
2021-05-11 14:06:22 +0100

    further tidying

--- training/unit-testing/go-lang/dependencyinjection/main.go
+++ training/unit-testing/go-lang/dependencyinjection/main.go
@@ -5,12 +5,12 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/ONSdigital/dp/training/unit-testing/go-lang/dependencyinjection/datastore"
-	service "github.com/ONSdigital/dp/training/unit-testing/go-lang/dependencyinjection/service"
+	"github.com/ONSdigital/dp/training/unit-testing/go-lang/dependencyinjection/service"
 	"os"
 	"strconv"
 )
 // main is a simple implementation of a database routine which takes in a value from the
-// user and get's the result from the database.
+// user and gets the result from the database.
 func main() {
 
 	conStr := "dbname=theDatabase sslmode=disable"
@@ -21,13 +21,13 @@ func main() {
 	// Create a datastore using the connection string
 	store := datastore.NewDatastore(d)
 	// Create the service by injecting the store as a dependency
-	service := &service.Service{Store: store}
+	serv := &service.Service{Store: store}
 	// The following code implements a simple command line app to read the ID as input
 	// and output the validity of the result of the entry with that ID in the database
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		ID, _ := strconv.Atoi(scanner.Text())
-		err := service.GetNumber(ID)
+		err := serv.GetNumber(ID)
 		if err != nil {
 			fmt.Printf("result invalid: %v", err)
 			continue
